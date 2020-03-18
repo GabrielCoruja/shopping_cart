da8936a277019e349c491ac9da2e97e4d305f74c
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-03-18 18:57:13 -0300

    Updates

---
+++ precache-manifest.5847f517ccd7566a462ed98dea5e87b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d010d9a58e0f5807b665917209fa241f",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "9c59ac8b04cb3c7c868f",
+    "url": "/shopping_cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "544d26be644a2d44a8d7",
+    "url": "/shopping_cart/static/js/2.1fb40e71.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/shopping_cart/static/js/2.1fb40e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c59ac8b04cb3c7c868f",
+    "url": "/shopping_cart/static/js/main.f854f4a4.chunk.js"
+  },
+  {
+    "revision": "6e2336b00352fcf782ea",
+    "url": "/shopping_cart/static/js/runtime-main.52653e7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping_cart/static/media/logo.5d5d9eef.svg"
+  }
+]);
